e624648f1e00eb0041c2896c087f927e29994a5f
NoUserToSeeMoveAlong <74629581+NoUserToSeeMoveAlong@users.noreply.github.com>
2021-02-24 20:12:08 +0100

    Create data.lua

---
+++ src/data.lua
@@ -0,0 +1,10 @@
+data.raw["utility-constants"]["default"].train_path_finding.train_stop_penalty = settings.startup["trainstop-penalty"].value
+data.raw["utility-constants"]["default"].train_path_finding.stopped_manually_controlled_train_penalty = settings.startup["manual-withplayer-penalty"].value
+data.raw["utility-constants"]["default"].train_path_finding.stopped_manually_controlled_train_without_passenger_penalty = settings.startup["manual-noplayer-penalty"].value
+data.raw["utility-constants"]["default"].train_path_finding.signal_reserved_by_circuit_network_penalty = settings.startup["circuit-penalty"].value
+data.raw["utility-constants"]["default"].train_path_finding.train_in_station_penalty = settings.startup["train-waitstation-penalty"].value
+data.raw["utility-constants"]["default"].train_path_finding.train_in_station_with_no_other_valid_stops_in_schedule = settings.startup["train-waitstation-nostops-penalty"].value
+data.raw["utility-constants"]["default"].train_path_finding.train_arriving_to_station_penalty = settings.startup["arriving-stop-penalty"].value
+data.raw["utility-constants"]["default"].train_path_finding.train_arriving_to_signal_penalty = settings.startup["arriving-signal-penalty"].value
+data.raw["utility-constants"]["default"].train_path_finding.train_waiting_at_signal_penalty = settings.startup["waiting-signal-penalty"].value
+data.raw["utility-constants"]["default"].train_path_finding.train_waiting_at_signal_tick_multiplier_penalty = settings.startup["waiting-at-signal-multiplier"].value
